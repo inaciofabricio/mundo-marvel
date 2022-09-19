0c77e8266a00927b26ec1bdc4d97fe33b1cd3ede
Inácio Fabrício Nunes de Sousa <inacio.fabricio.nunes@gmail.com>
2022-09-19 16:03:26 -0300

    Em andamento

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AppRouter from './components/AppRouter';
+
+const App = () => {
+
+  return (
+    <div className="body">
+        <AppRouter />
+    </div>
+  );
+}
+
+export default App;
